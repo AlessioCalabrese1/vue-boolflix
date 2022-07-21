12d4c68c08b13625569c4ffeb19cc6bcc20b0fd5
AlessioCalabrese1 <alessiocalabrese07@gmail.com>
2022-07-21 17:47:20 +0200

    Milestone 2: insertion of flags

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -12,6 +12,10 @@
             <p class="card-text">{{ cardElement.name }}</p>
             <p class="card-text">{{ cardElement.original_name }}</p>
             <p class="card-text">{{ cardElement.original_language }}</p>
+            <p class="card-text">{{ cardElement.origin_country}}</p>
+            <div class="flag">
+                <img class="img-fluid" :src="require(`../assets/flags/${showFlag(cardElementType, cardElement.origin_country)}.png`)" alt="flag">
+            </div>
             <p class="card-text">{{ cardElement.vote_average }}</p>
         </div>
     </div>
@@ -29,9 +33,47 @@ export default {
             type: String,
             required: true
         }
+    },
+
+    data: function(){
+        return{
+            flags:[
+                {
+                    language: "it",
+                    country: "IT",
+                    flagImg: "/assets/flags/ITflag.png"
+                },
+                {
+                    language: "en",
+                    country: "US",
+                    flagImg: "/assets/flags/USAflag.png"
+                },
+            ],
+
+            backFallImgFlag: "fallback",
+        }
+    },
+
+    methods:{
+        showFlag(elementType, nationalityIdentifier){
+            if (elementType === "tvShow") {
+                console.log(nationalityIdentifier)
+                for (let index = 0; index < this.flags.length; index++) {
+                    if (this.flags[index].country == nationalityIdentifier) {
+                        return this.flags[index].language;
+                    }
+                }
+                return this.backFallImgFlag;
+            }
+        },
     }
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+
+    .flag{
+        width: 20px;
+    }
+
 </style>
