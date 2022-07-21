3d071fd4c6670fb9d0cd36fd39fa264c320564df
AlessioCalabrese1 <alessiocalabrese07@gmail.com>
2022-07-21 14:42:24 +0200

    Milestone 1: passage through emit

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <Header />
+    <Header @filters="viewingFilteredContent" />
     <Main />
   </div>
 </template>
@@ -12,11 +12,34 @@ import Main from './components/Main.vue';
 
 export default {
   name: 'App',
+
+  data: function(){
+    return{
+      keyAPI: "404b2ed5e305c129916cb234a784ab73",
+    }
+  },
+
   components: {
-    HelloWorld,
     Header,
     Main
-}
+  },
+
+  methods:{
+    getMovie(){
+      axios.get("https://api.themoviedb.org/3/search/movie?api_key=404b2ed5e305c129916cb234a784ab73&query=ritorno+al+futuro")
+      .then( (result) => {
+        console.log(result.data.results)
+      })
+    },
+
+    viewingFilteredContent(filter){
+      console.log(filter);
+    }
+  },
+
+  created(){
+    this.getMovie();
+  }
 }
 </script>
 
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,13 +1,21 @@
 <template>
-  
+    <div class="input-group mb-3">
+        <button class="btn btn-outline-secondary" type="button" id="button-addon1" @click="$emit('filters', filter)">Button</button>
+        <input type="text" class="form-control" placeholder="" aria-label="Example text with button addon" aria-describedby="button-addon1"
+        v-model="filter" @keyup.enter="$emit('filters', filter)">
+    </div>
 </template>
 
 <script>
 export default {
-
+    data: function(){
+        return{
+            filter: "",
+        }
+    }
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
 
 </style>
